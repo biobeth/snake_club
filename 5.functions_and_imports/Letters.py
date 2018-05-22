ae70b5e6adab47d485ee9e60e84be72301b7e051
biobeth <bethanyjose@gmail.com>
2018-05-22 18:35:32 +1200

    organising

---
+++ 5.functions_and_imports/Letters.py
@@ -0,0 +1,14 @@
+def SIGNATURE():
+    print "Harry Biggs"
+def REJECT(NAME):
+    print "Dear", NAME
+    print "I am sorry to inform you that you do not have the job"
+    print "Yours sincerely"
+    SIGNATURE()
+REJECT("BRUCE")
+def ACCEPT(NAME):
+    print "Dear", NAME
+    print "I am pleased to inform you that you have the job"
+    print "Yours sincerely"
+    SIGNATURE
+ACCEPT("AMANDA")
---
+++ 5.functions_and_imports/is even.py
@@ -0,0 +1,6 @@
+def is_even(n):
+    if n/2 == True:
+        print True
+    else:
+        print False
+is_even(6)
